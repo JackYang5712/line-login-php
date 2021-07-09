9f940ba36adcb39b83f191624c7bfee27d6d93aa
JackYang5712 <84710942+JackYang5712@users.noreply.github.com>
2021-07-09 10:50:39 +0800

    Update and rename config0.php to config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+//設定值
+define("CLIENT_ID", '1656185384');
+define("CLIENT_SECRET", '11ce9d951bde0a001de8f3ab60f4fabf');
+define("REDIRECT_URI", 'https://line-login-starter-20210709.herokuapp.com/auth');//登入後返回位置
+define("SCOPE", 'openid%20profile%20email');//授權範圍以%20分隔 可以有3項openid，profile，email
